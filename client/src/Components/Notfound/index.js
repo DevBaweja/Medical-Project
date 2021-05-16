ec26f1a0308b55120bc8e754ec2a2f8b234e0809
KrishnaZalani <zalani.krishna@gmail.com>
2021-05-16 18:01:33 +0530

    Fixed 404 Not found page

--- client/src/Components/Notfound/index.js
+++ client/src/Components/Notfound/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import './style.css';
+import nfimage from '../../Images/404.png';
 
 class Notfound extends React.Component {
 	render() {
 		return (
-			<div className="notFound">
-				<div className="c">
-					<div className="_404">404</div>
-					<div className="_1">THE PAGE</div>
-					<div className="_2">WAS NOT FOUND</div>
-					<a className="btn not_btn" href="/">
-						BACK TO HOME
-					</a>
-				</div>
+			// <div className="notFound">
+			// 	<div className="c">
+			// 		<div className="_404">404</div>
+			// 		<div className="_1">THE PAGE</div>
+			// 		<div className="_2">WAS NOT FOUND</div>
+			// 		<a className="btn not_btn" href="/">
+			// 			BACK TO HOME
+			// 		</a>
+			// 	</div>
+			// </div>
+
+			<div className="container text-center">
+				<img src={nfimage} className="nf-image" />
 			</div>
 		);
 	}
