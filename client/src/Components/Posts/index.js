4fe6a644d7b5d46e70c6a16632269e798277a160
Jayesh-30 <18ucs090@lnmiit.ac.in>
2021-05-24 16:43:06 +0530

    posts update

--- client/src/Components/Posts/index.js
+++ client/src/Components/Posts/index.js
@@ -4,6 +4,11 @@ import './style.css';
 import Posts from './Posts';
 import { Link, withRouter } from 'react-router-dom';
 import { signout, isAuthenticated } from '../../Api';
+import postsSvg from '../../Images/Posts/posts_bg.svg';
+import postsBg from '../../Images/Posts/posts_bg_curve.png';
+import postIcon from '../../Images/Posts/post-icon.svg';
+import tagIcon from '../../Images/Posts/tag-icon.svg';
+import verifiedIcon from '../../Images/Posts/verified-icon.svg';
 
 const isActive = (history, path) => {
 	if (history.location.pathname === path) return { color: 'white', fontWeight: '600' };
@@ -13,14 +18,52 @@ class ViewPosts extends React.Component {
 	render() {
 		const { history } = this.props;
 		return (
-			<>
+			<div className= "posts">
+				<img className= "posts-bg-img" src= {postsBg}></img> 
+				<div className= "container posts-start">
+					<div className= "row posts_intro">
+						<div className= "col">
+							<div className= "posts_title">
+								<div className= "motto">Posts</div>
+							</div>
+							<div className= "posts_content">
+								<div className= "content-line">View all type of posts.</div>
+								<div className= "content-line">Like posts and share personal experience.</div>
+								<div className= "content-line">Ask suggestions from the community and doctors.</div>
+							</div>
+						</div>
+						<div className= "col posts-svg">
+							<img src= {postsSvg} class= "posts-svg_svg"></img>
+						</div>
+					</div>
+					<div className= "row">
+						<div className= "col">
+							<div class="posts_card">
+								<img src= {postIcon} class="posts-card_icon posts-card_icon-post"></img>
+								<div class="posts-card_heading">500+</div>
+								<div class="posts-card_text">Posts</div>
+							</div>
+						</div>
+						<div className= "col">
+							<div class="posts_card">
+								<img src= {tagIcon} class="posts-card_icon"></img>
+								<div class="posts-card_heading">100+</div>
+								<div class="posts-card_text">Tags</div>
+							</div>
+						</div>
+						<div className= "col">
+							<div class="posts_card">
+								<img src= {verifiedIcon} class="posts-card_icon"></img>
+								<div class="posts-card_heading">
+									90% +
+								</div>
+								<div class="posts-card_text">Verified Posts</div>
+							</div>
+						</div>
+					</div>
+				</div>
 				<div className="container mb-5">
 					<div>
-						<h2 className="mt-5">Posts</h2>
-						<p className="lead lead-post">
-							{' '}
-							View all type of post like share experience, asking suggestions
-						</p>
 						<div className="text-right">
 							<p className="btn btn-outline-primary btn-sm postButton">
 								<Link
@@ -37,7 +80,7 @@ class ViewPosts extends React.Component {
 				<div className="container mob-table" style={{ overflow: 'scroll' }}>
 					<Posts />
 				</div>
-			</>
+			</div>
 		);
 	}
 }
